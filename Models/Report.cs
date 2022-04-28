d949e65dc651729b4a516f280c072b6a303e0e30
tanya.sharma1 <tanya.sharma@testingxperts.com>
2022-04-28 15:28:33 +0530

    Report controller commit

---
+++ Models/Report.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LivingAssistance2.Models
+{
+    public class Report
+    {
+        [Required]
+        public Searching Searchtype { get; set; }
+        [Required]
+        public String SearchData { get; set; }
+    }
+    public enum Searching
+    {
+        Patient,
+        CareGiver
+    }
+}
+
+
