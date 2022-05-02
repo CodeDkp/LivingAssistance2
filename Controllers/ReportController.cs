8b011f1dc72bd5ca3b61a7901635cde9a11a3763
deepak.patel2 <deepak.patel@testingxperts.com>
2022-05-02 17:34:40 +0530

    Search Trail 1

--- Controllers/ReportController.cs
+++ Controllers/ReportController.cs
@@ -1,18 +1,40 @@
 ﻿using LivingAssistance2.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
 
 namespace LivingAssistance2.Controllers
 {
     public class ReportController : Controller
     {
+        private readonly ORGContext _context;
+
+        public ReportController(ORGContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
-            return View();
+            
+           return View();
+                       
         }
 
         public IActionResult Display(Report R)
         {
-            return View();
+        //    if(true)
+        //    {
+        //        string usertype = "Caregiver", name = "minu", address= "Hydrabad", date1= "2021-03-27", date2= "2022-11-27";
+        //       var data = _context.PatientDetails.FromSqlRaw("[dbo].[Search_Procedure] @usertype,@name, @Hydrabad,@2021-03-27,@2022-11-27", new SqlParameter("usertype,name,address,date1,date2", usertype,name, address, date1, date2)).ToList();
+        //        return View(data);
+        //    }
+        //    else if(false)
+        //    {
+        //        string Name = "Raaj";
+        //        List<PatientDetail>? data = _context.PatientDetails.FromSqlRaw("[dbo].[SearchPatient] @Name", new SqlParameter("Name", Name)).ToList();
+        //        return View();
+        //    }
+              return View();
         }
     }
 }
