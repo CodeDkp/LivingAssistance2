d7f3a57bb322e698c92cf03f93ddbe80f7d7754a
tanya.sharma1 <tanya.sharma@testingxperts.com>
2022-05-16 12:23:37 +0530

    done

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -1,4 +1,5 @@
 ﻿using LivingAssistance2.Models;
+using LivingAssistance2.Security;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
@@ -7,26 +8,82 @@ namespace LivingAssistance2.Controllers
 {
     public class LoginController : Controller
     {
+	    private readonly ORGContext _context;
+
+        //constructor
+        public LoginController (ORGContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
+
+        //Log in page
         [HttpGet]
-        public IActionResult LogIn()
+        public IActionResult LogInPage()
         {
             return View();
         }
+
         [HttpPost]
-        public IActionResult LogIn(Login l)
+        public IActionResult LogInPage(Login L)
         {
-            //if(! string.IsNullOrEmpty(login.Username)&&string.IsNullOrEmpty(login.Password))
-            //{
+            bool userexists = CheckExitingUser(L);
+            if (userexists)
+            {
+                return View("Index","Home");
+            }
+            else
+            {
+                return RedirectToAction("LogInPage");
+            }
+        }
 
-            //}
 
+        private bool CheckExitingUser(Login L)
+        {
+            try
+            {
+                //Get Salt Key From Database
+                var getUser = (from s in _context.UserDetails where s.Username == L.Username select s).FirstOrDefault();
+
+            if (getUser != null)
+            {
+                //Encrypt Password according to Salt Key
+                var encodedPasswordString = Encrypt.EncodePassword(L.Password, getUser.Saltvalue);
+                //Check Login Detail User Name Or Password    
+                var query = (from s in _context.UserDetails where (s.Username == L.Username) && s.Password.Equals(encodedPasswordString) select s).FirstOrDefault();
+                if (query != null)
+                {
+                        //User Exist
+                       // Authorization(L,query.UserTypeId);    //Authorize 
+                    return true;
+                }
+                return false;
+            }
+            else
+            {
+                    //Wrong User Id or Password
+                    return false;
+            }
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+
+
+        private void Authorization(Login l, string Usertype)
+        {
+            
             ClaimsIdentity identity = null;
             bool isAuthenticate = false;
-            if(l.Username == "" && l.Password=="")//get grom Database
+            //For Patient
+            if (Usertype.Contains("P"))
             {
                 identity = new ClaimsIdentity(new[]
                 {
@@ -35,7 +92,8 @@ namespace LivingAssistance2.Controllers
                 }, CookieAuthenticationDefaults.AuthenticationScheme);
                 isAuthenticate = true;
             }
-            if (l.Username == "" && l.Password == "")//get grom Database
+            //For Care Giver
+            if (Usertype.Contains("C"))
             {
                 identity = new ClaimsIdentity(new[]
                 {
@@ -44,7 +102,8 @@ namespace LivingAssistance2.Controllers
                 }, CookieAuthenticationDefaults.AuthenticationScheme);
                 isAuthenticate = true;
             }
-            if (l.Username == "" && l.Password == "")//get grom Database
+            //For Employee
+            if (Usertype.Contains("E"))
             {
                 identity = new ClaimsIdentity(new[]
                 {
@@ -53,14 +112,14 @@ namespace LivingAssistance2.Controllers
                 }, CookieAuthenticationDefaults.AuthenticationScheme);
                 isAuthenticate = true;
             }
+
             if (isAuthenticate)
             {
                 var principle = new ClaimsPrincipal(identity);
                 //var login = HttpContent.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principle);
-                return RedirectToAction("Index", "Home");
+               
             }
-
-            return View();
         }
+        
     }
 }
